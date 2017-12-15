4c67c91ae672badef916d0ad72ebe218b8242182
bsantos01 <b_daniel@netcabo.pt>
2017-12-15 00:39:52 +0000

    dbworking

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.net.SocketException;
 import java.sql.SQLException;
 import java.net.SocketException;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
