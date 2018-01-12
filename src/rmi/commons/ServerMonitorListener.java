23271582d3022ede1dc2cf7e239e38d20e0209fe
simaohsferreira <simaohsferreira@gmail.com>
2018-01-12 16:36:53 +0000

    rmi update, barebones structure connecting

--- src/rmi/commons/ServerMonitorListener.java
+++ src/rmi/commons/ServerMonitorListener.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rmi.commons;
 
-import rmi.client.*;
-
-/**
- *
- * @author simao
- */
 public interface ServerMonitorListener extends java.rmi.Remote {
 
     public void printServers() throws java.rmi.RemoteException;
+
+    //print LoggedUsers
+    //print Pairs //inCreation //inGame
+    //print history
 }
